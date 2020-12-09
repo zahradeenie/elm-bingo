877697892fe292d8fae1dfb1f919c88421b58e65
Zahra <zahradeenie@gmail.com>
2020-12-09 22:35:55 +0100

    feat: add funs for header and footer and add view function to main

--- Bingo.elm
+++ Bingo.elm
@@ -1,16 +1,36 @@
 module Bingo exposing (..)
 
-import Html
+import Html as H
+import Html.Attributes as HA
 
 
 playerInfo name gameNumber =
     name ++ " - Game #" ++ (toString gameNumber)
 
+viewPlayer name gameNumber =
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> H.text
+    in
+        H.h2 [ HA.id "info", HA.class "classy" ] [ playerInfoText ]
 
-playerInfoText name gameNumber =
-    playerInfo name gameNumber
-        |> String.toUpper
-        |> Html.text
+viewHeader title =
+    H.header []
+        [ H.h1 [] [ H.text title ] ]
+
+viewFooter =
+    H.footer []
+        [ H.a [ HA.href "https://github.com/zahradeenie" ] [ H.text "Made by Zahra" ] ]
+
+
+view =
+    H.div [ HA.class "content" ]
+        [ viewHeader "Buzzword Bingo"
+        , viewPlayer "Zahra" 3
+        , viewFooter
+        ]
 
 main =
-    playerInfoText "Zahra" 3
+    view
