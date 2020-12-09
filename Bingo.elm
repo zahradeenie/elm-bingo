4f1cfac58cb52d379454c94654e62c916c8c7b9d
Zahra <zahradeenie@gmail.com>
2020-12-09 23:00:06 +0100

    chore: add type annotations to funs

--- Bingo.elm
+++ Bingo.elm
@@ -3,10 +3,12 @@ module Bingo exposing (..)
 import Html as H
 import Html.Attributes as HA
 
-
+playerInfo : String -> Int -> String
 playerInfo name gameNumber =
     name ++ " - Game #" ++ (toString gameNumber)
 
+
+viewPlayer : String -> Int -> H.Html msg
 viewPlayer name gameNumber =
     let
         playerInfoText =
@@ -16,15 +18,19 @@ viewPlayer name gameNumber =
     in
         H.h2 [ HA.id "info", HA.class "classy" ] [ playerInfoText ]
 
+
+viewHeader : String -> H.Html msg
 viewHeader title =
     H.header []
         [ H.h1 [] [ H.text title ] ]
 
+
+viewFooter : H.Html msg
 viewFooter =
     H.footer []
         [ H.a [ HA.href "https://github.com/zahradeenie" ] [ H.text "Made by Zahra" ] ]
 
-
+view : H.Html msg
 view =
     H.div [ HA.class "content" ]
         [ viewHeader "Buzzword Bingo"
@@ -32,5 +38,7 @@ view =
         , viewFooter
         ]
 
-main =
+
+main : H.Html msg
+main = 
     view
